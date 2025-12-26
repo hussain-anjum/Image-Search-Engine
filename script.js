fed14722ffad01a94229559758ba20a277b5e9d9
hussain-anjum <haratul14@gmail.com>
2025-12-26 21:01:22 +0600

    Fix image load for new search

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const apiKey = config.API_KEY;
 const searchForm = document.getElementById("search-form");
 const searchBox = document.getElementById("search-box");
 const searchResult = document.getElementById("search-result");
-const searchMoreBtn = document.getElementById("show-more-btn");
+const showMoreBtn = document.getElementById("show-more-btn");
 
 let keyword = "";
 let page = 1;
@@ -11,16 +11,39 @@ let page = 1;
 async function searchImages() {
   keyword = searchBox.value;
   //   console.log(keyword);
-  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${apiKey}`;
+  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${apiKey}&per_page=12`;
 
   const response = await fetch(url);
   const data = await response.json();
+  // console.log(data);
 
-  console.log(data);
+  if (page == 1) {
+    searchResult.innerHTML = "";
+  }
+  const results = data.results;
+
+  results.map((result) => {
+    const image = document.createElement("img");
+    image.src = result.urls.small;
+
+    const imageLink = document.createElement("a");
+    imageLink.href = result.links.html;
+    imageLink.target = "_blank";
+
+    imageLink.appendChild(image);
+
+    searchResult.appendChild(imageLink);
+  });
+  showMoreBtn.style.display = "block";
 }
 
 searchForm.addEventListener("submit", (e) => {
   e.preventDefault();
   page = 1;
+  searchImages();
+});
+
+showMoreBtn.addEventListener("click", () => {
+  page++;
   searchImages();
 });
